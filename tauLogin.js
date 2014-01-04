30ec83bc6e8fe4b35e8fc20fcb62b5b04df22c4b
livnekes <livnek@gmail.com>
2014-01-04 22:42:41 +0200

    add redirect url

--- tauLogin.js
+++ tauLogin.js
@@ -10,17 +10,21 @@ javascript:(function(){
     document.getElementsByName("btnishur")[0].click();
     return;
   }
-  var t=document.getElementsByTagName("iframe")[0].contentDocument.getElementsByTagName("iframe")[0];
-  var e=t.contentDocument.getElementsByTagName("form");
-  if(e!=null&&e.length==1){
-    var n=t.contentDocument;
-    n.getElementsByName("Ecom_User_ID")[0].value=username;
-    n.getElementsByName("Ecom_Password")[0].value=password;
-    n.getElementsByName("Ecom_User_Pid")[0].value=id;
-    n.getElementsByTagName("a")[0].click();
-    return;
+  var i=document.querySelector('iframe');
+  if (i!=null&&i.contentDocument!=null)
+  {
+    var t=i.contentDocument.querySelector('iframe');
+    if (t!=null)
+      e=t.contentDocument.querySelector("form");
+    if(e!=null){
+      var n=t.contentDocument;
+      n.getElementsByName("Ecom_User_ID")[0].value=username;
+      n.getElementsByName("Ecom_Password")[0].value=password;
+      n.getElementsByName("Ecom_User_Pid")[0].value=id;
+      n.getElementsByTagName("a")[0].click();
+      return;
+    }
   }
-  else{
-    console.log("The site is not supported!");
-  }
+  console.log("The site is not supported!");
+  window.location='https://idp.tau.ac.il/nidp/saml2/sso';
 })();
